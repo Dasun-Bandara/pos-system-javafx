a62ff8304e8d78d7d726a02515a03b5618e31d83
Dasun Bandara <dasun8517@gmail.com>
2024-01-19 14:39:09 +0530

    Add UI Design

---
+++ src/main/java/lk/ijse/dep11/pos/controller/MainFormController.java
@@ -0,0 +1,25 @@
+package lk.ijse.dep11.pos.controller;
+
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class MainFormController {
+    public AnchorPane root;
+    public ImageView imgCustomer;
+    public ImageView imgItem;
+    public ImageView imgOrder;
+    public ImageView imgViewOrders;
+    public Label lblMenu;
+    public Label lblDescription;
+
+    public void navigate(MouseEvent mouseEvent) {
+    }
+
+    public void playMouseEnterAnimation(MouseEvent mouseEvent) {
+    }
+
+    public void playMouseExitAnimation(MouseEvent mouseEvent) {
+    }
+}
